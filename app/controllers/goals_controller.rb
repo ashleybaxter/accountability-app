ca56d733c2f03d5fa2b1d8f9dd3c26361d2ac3d0
ashleybaxter <ashleybaxter@me.com>
2014-10-01 19:53:34 +0100

    Goal model + controller
    Create functionality implemented.

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,23 @@
+class GoalsController < ApplicationController
+  def index
+    @goals = Goal.all
+  end
+  
+  def new
+    @goal = Goal.new
+  end
+  
+  def create
+    @goal = Goal.new(app_params)
+		if @goal.save
+   			redirect_to goals_path
+  		else
+    		render('new')
+  		end
+  end
+  
+	def app_params
+    params.require(:goal).permit(:title)
+ 	end
+ 	
+end
