039126def9c6a792827fa0a881e74799dc925572
ashleybaxter <ashleybaxter@me.com>
2014-10-20 22:15:39 +0100

    Removed secret key from .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccountabilityApp::Application.config.secret_key_base = '4b9f9710b67bdf470fbefaeee27adbfd8fd050e9e0a71b74bb1560f1f6565d60ebdff36a5ada9f6fc4e763b773d19d91e851b90adf6d5d5070820b335ad362fe'
