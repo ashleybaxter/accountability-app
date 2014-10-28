df55cae496c9f5cfc488ed3e63ef339cb6aab890
ashleybaxter <ashleybaxter@me.com>
2014-10-28 15:19:15 +0000

    Routes for failed authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   provider :twitter, 'zJtxW5Q2jAs7RuDNC1TLDWkI3', 'MB7JXGfTEcYiXl2TEWyMUMTJrzek5InQkHfTy59I7KIC9v7gjz'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ AccountabilityApp::Application.routes.draw do
   root :to => "goals#index" 
   get 'auth/twitter/callback' => 'sessions#create'
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/failure', to: redirect('/')
   
   resources :goals do
     get 'tomorrow', :on => :collection
