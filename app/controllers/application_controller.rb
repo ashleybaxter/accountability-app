348c83fcefbaa81d86851ad9df9f605bd015f7bd
ashleybaxter <ashleybaxter@me.com>
2014-11-03 11:31:03 +0000

    last_seen_at for User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_last_seen_at, if: proc { |p| current_user }
+  
   protect_from_forgery with: :exception
   
   private
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def set_last_seen_at
+    current_user.update_attribute(:last_seen_at, Time.now)
   end
   
   helper_method :current_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028153611) do
+ActiveRecord::Schema.define(version: 20141103112818) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141028153611) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "last_seen_at"
   end
 
 end
